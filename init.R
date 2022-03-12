a92440ffdd2fea00ce6af6b82e8c8cd71189691d
Morgan Rosenberg <morgan.ar.rosenberg@gmail.com>
2022-03-12 02:04:21 -0800

    f

--- init.R
+++ init.R
@@ -9,5 +9,5 @@ options(repos=r)
 # ======================================================================
 
 # packages go here
-install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes', 'tidyverse', 'ggplot2', 'plotly'))
+install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes', 'tidyverse', 'ggplot2', 'plotly', 'shiny'))
 remotes::install_github('facultyai/dash-bootstrap-components@r-release')
